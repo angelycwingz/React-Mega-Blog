cf8b58fedec729f0aa656a6bb45341657263381d
MUKUL ARORA <aruntandon.tandon007@gmail.com>
2025-04-20 00:46:13 +0530

    Appwrite Auth service finished

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import appwriteUrl from "./conf/conf"
+
+function App() {
+  console.log(appwriteUrl);
+
+  return (
+    <>
+      <h1>A Japanese blog app with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
